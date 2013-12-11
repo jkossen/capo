d33d83f8f8a475ac6edd71b901d8f150ce1aed54
Jochem Kossen <jochem@jkossen.nl>
2013-12-11 14:10:10 +0100

    Better import scripts

--- scripts/import.sh
+++ scripts/import.sh
@@ -1,42 +1,73 @@
 #!/bin/sh
+
+#
+# configuration
+WDIR="$(dirname $0)"
+cd "$WDIR"
+
 TMPFILE=$(/bin/mktemp /tmp/capo.XXXXXXXX)
-POSTFILE='/var/www/jkossen.nl/public/capo/scripts/capo_export.post'
 BASE_URL="$1"
 URL="${BASE_URL}capo_export.php"
-DBUSER="capo"
-DB="capo"
-DBPASS="ddldKLDwdjkldDnmvcsv923ejkdlas9;"
+DBUSER="$(grep database_user ../app/config/parameters.yml |awk '{print $2}' |sed -e 's/"//g')"
+DB="$(grep database_name ../app/config/parameters.yml |awk '{print $2}' |sed -e 's/"//g')"
+DBPASS="$(grep database_password ../app/config/parameters.yml |awk '{print $2}' |sed -e 's/"//g')"
+RETRIEVAL_CODE="$(grep capo_retrieval_code ../app/config/parameters.yml |awk '{print $2}' |sed -e 's/"//g')"
+
+export PATH="$PATH:/bin:/sbin:/usr/bin:/usr/local/bin"
+
+#
+# functions
+cleanup() {
+	if [ -f "$TMPFILE" ]; then
+        rm -f "$TMPFILE"
+    fi
+}
+
+exit_err() {
+    echo "ERROR: $1" >&2
+    cleanup
+    exit $2
+}
+
+exit_int() {
+    echo "Signal caught. Exiting ..." >&2
+    cleanup
+    exit 2
+}
+
+retrieve_data() {
+    wget -t 1 --timeout 120 -O "$TMPFILE" --post-data="code=$RETRIEVAL_CODE" "$1"
+    
+    if [ $? != 0 ]; then
+        exit_err "ERROR: failed to retrieve $URL" 1
+    fi
+}
+
+integrate_data() {
+    if [ -s "$TMPFILE" ]; then
+	    mysql -u $DBUSER --password="$DBPASS" $DB < $TMPFILE
+
+	    if [ $? != 0 ]; then
+		    exit_err "ERROR: mysql import failed" 1
+        else
+            IMPORT_DATE=$(date +"%Y-%m-%d %H:%M:%S")
+            SQL="UPDATE cacti_instance SET import_date = '$IMPORT_DATE', queue_import = 0 WHERE base_url = '$BASE_URL'"
+
+            mysql -u $DBUSER --password="$DBPASS" $DB -e "$SQL"
+	    fi
+    else
+	    exit_err "ERROR: size of SQL file $TMPFILE is 0" 1
+    fi
+}
+
+#
+# run
+trap exit_int HUP INT TERM
 
 if [ -z "$BASE_URL" ]; then
-    echo 'ERROR: BASE_URL not set.' >&2
-    exit 1
+    exit_err 'BASE_URL not set.' 1
 fi
 
-/usr/bin/wget -t 1 --timeout 120 -O "$TMPFILE" --post-data="$(cat $POSTFILE)" "$URL"
-
-if [ $? != 0 ]; then
-	echo "ERROR: failed to retrieve $URL" >&2
-	rm "$TMPFILE"
-	exit 1
-fi
-
-if [ -s "$TMPFILE" ]; then
-	/usr/bin/mysql -u $DBUSER --password="$DBPASS" $DB < $TMPFILE
-
-	if [ $? != 0 ]; then
-		echo "ERROR: mysql import failed" >&2
-    else
-        IMPORT_DATE=$(date +"%Y-%m-%d %H:%M:%S")
-        SQL="UPDATE cacti_instance SET import_date = '$IMPORT_DATE', queue_import = 0 WHERE base_url = '$BASE_URL'"
-
-        echo $SQL
-        /usr/bin/mysql -u $DBUSER --password="$DBPASS" $DB -e "$SQL"
-	fi
-else
-	echo "ERROR: size of SQL file $TMPFILE is 0" >&2
-	rm "$TMPFILE"
-	exit 1
-fi
-
-rm "$TMPFILE"
-
+retrieve_data "$URL"
+integrate_data
+cleanup
--- scripts/import_all.sh
+++ scripts/import_all.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-WDIR="/var/www/jkossen.nl/public/capo/scripts"
-DB="capo"
-DBUSER="capo"
-DBPASS="ddldKLDwdjkldDnmvcsv923ejkdlas9;"
-
-OLD_DATE=$(date -d "1 hour ago" +"%Y-%m-%d %H:%M")
-
+WDIR="$(dirname $0)"
 cd "$WDIR"
 
-mysql -u "$DBUSER" --password="$DBPASS" -e "SELECT base_url FROM cacti_instance WHERE active = 1 AND (import_date <= '$OLD_DATE' OR queue_import = 1)" "$DB" |grep -v 'base_url' |while read BASE_URL; do
-    ./import.sh "$BASE_URL"
-done
+DBUSER="$(grep database_user ../app/config/parameters.yml |awk '{print $2}' |sed -e 's/"//g')"
+DB="$(grep database_name ../app/config/parameters.yml |awk '{print $2}' |sed -e 's/"//g')"
+DBPASS="$(grep database_password ../app/config/parameters.yml |awk '{print $2}' |sed -e 's/"//g')"
+STALE_DATE=$(date -d "1 hour ago" +"%Y-%m-%d %H:%M")
+SQL="SELECT base_url FROM cacti_instance WHERE active = 1 AND (import_date <= '$STALE_DATE' OR queue_import = 1)"
 
+export PATH="$PATH:/bin:/sbin:/usr/bin:/usr/local/bin"
+
+mysql -u "$DBUSER" --password="$DBPASS" -e "$SQL" "$DB" |grep -v 'base_url' |while read BASE_URL; do
+    "$WDIR/import.sh" "$BASE_URL"
+done

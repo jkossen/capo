056229b8682f54505fd8922cb4b21b85ae23a3e5
Jochem Kossen <jochem@jkossen.nl>
2015-06-10 13:41:19 +0200

    Add test-bootstrap.php

---
+++ app/test-bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+
+if (isset($_ENV['BOOTSTRAP_CLEAR_CACHE_ENV'])) {
+    passthru(sprintf(
+        'php "%s/console" cache:clear --env=%s --no-warmup',
+        __DIR__,
+        $_ENV['BOOTSTRAP_CLEAR_CACHE_ENV']
+    ));
+    passthru(sprintf(
+        'php "%s/console" doctrine:schema:create --env=test',
+        __DIR__
+    ));
+    passthru(sprintf(
+        'php "%s/console" doctrine:fixtures:load -n --env=test',
+        __DIR__
+    ));
+}
+
+/* force loading of class */
+if (class_exists('PHPUnit_Util_ErrorHandler', true)) {
+}
+
+//require_once(__DIR__ . '/bootstrap.php.cache');
+require_once(__DIR__ . '/../vendor/symfony/symfony/src/Symfony/Bridge/PhpUnit/bootstrap.php');
+?>
